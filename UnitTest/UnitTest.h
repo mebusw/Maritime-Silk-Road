259e9e10825d502bf6f956749777fc61c67b194e
mebusw <mebusw@bogon.(none)>
2012-01-26 11:09:42 +0800

    reset Unit test

---
+++ UnitTest/UnitTest.h
@@ -0,0 +1,13 @@
+//
+//  UnitTest.h
+//  UnitTest
+//
+//  Created by mebusw on 12-1-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTest : SenTestCase
+
+@end
