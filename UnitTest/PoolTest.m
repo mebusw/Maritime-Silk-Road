b5d168a43d4f56c84fc2d5fb02add64fdeb4e7aa
mebusw <mebusw@bogon.(none)>
2012-01-26 11:17:06 +0800

    reset Unit test

---
+++ UnitTest/PoolTest.m
@@ -0,0 +1,19 @@
+//
+//  PoolTest.m
+//  MaritimeSilkRoad
+//
+//  Created by mebusw on 12-1-26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PoolTest.h"
+
+@implementation PoolTest
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
