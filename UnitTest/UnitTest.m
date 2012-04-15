af4be626e6b6fcdbb24d7b82fe9bd1b78cdd6f93
mebusw <mebusw@163.com>
2012-04-15 21:49:57 +0800

    rename groups add global macros

--- UnitTest/UnitTest.m
+++ UnitTest/UnitTest.m
@@ -34,4 +34,25 @@
     DLog(@"rnd=%f %d", f, (NSInteger)f);
 }
 
+/*
+-(void) testOCMockExample {
+    MasterViewController *controller = [[MasterViewController alloc] init];
+    NSIndexPath *dummyIndexPath = [NSIndexPath indexPathForRow:3 inSection:1];
+    id tableViewMock = [OCMockObject mockForClass:[UITableView class]];
+    [[tableViewMock expect] deleteRowsAtIndexPaths:[NSArray arrayWithObject:dummyIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+    
+    // Invoke functionality to be tested
+    // If you want to see the test fail you can, for example, change the editing style to UITableViewCellEditingStyleNone. In
+    // that case the method in the controller does not make a call to the table view and the mock will raise an exception when
+    // verify is called further down.
+    
+    [controller tableView:tableViewMock commitEditingStyle:UITableViewCellEditingStyleDelete forRowAtIndexPath:dummyIndexPath];
+    
+    // Verify that expectations were met
+    
+    [tableViewMock verify];
+    
+    
+}
+*/
 @end
